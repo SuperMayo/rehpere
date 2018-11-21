801f97cece39f6b082e23c5c592859e79bee3154
Antoine Mayerowitz <antoine@mayerowitz.io>
2018-11-21 11:15:20 +0100

    :bug: Solves bad master link behavior on mobile.
    Cancel the Hover behavior of master nav link in favor of a classical dropdown button. This allows for normal behavior on mobile.

--- assets/js/makeActiveLinks.js
+++ assets/js/makeActiveLinks.js
@@ -1,6 +1,7 @@
 var links = document.querySelectorAll('.nav-link');
+var pageUrl = window.location.href.replace(window.location.hash, "");
 for (var i = 0; i < links.length; i++) {
-	if (links[i].href == window.location.href){
+	if (links[i].href == pageUrl){
 		links[i].classList.add("active");
 	}
 }
